6ddbffb08bbc7a4a732dd59e057502607331c025
github-nqvnlc <locnvgcd18350@fpt.edu.vn>
2021-11-25 21:33:40 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Content from './Content'
 
 function App() {
+
+  const [job, setJob] = useState('')
+  const [jobs, setJobs] = useState(() => {
+    const storageJobs = JSON.parse(localStorage.getItem('jobs'))
+    return storageJobs
+      ?? []
+  })
+
+  const handleSub = () => {
+    setJobs(prev => {
+      const newJobs = [...prev, job]
+
+      // save to local storage
+      const jsonJobs = JSON.stringify(newJobs)
+      localStorage.setItem('jobs', jsonJobs)
+      return newJobs
+    })
+    setJob('')
+  }
+
+  const handleDel = () => {
+    setJobs(() => {
+      const newJobs = []
+      // save to local storage
+      const jsonJobs = JSON.stringify(newJobs)
+      localStorage.setItem('jobs', jsonJobs)
+      return newJobs
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 32 }}>
+      <h1>List To Do</h1>
+      <input value={job} onChange={e => setJob(e.target.value)} />
+      <button onClick={handleSub}>Add</button>
+
+      <ul>
+        {jobs.map((job, index) => (
+          <li key={index}>{job}</li>
+        )
+        )}
+
+      </ul>
+
+      <button onClick={handleDel}>Delete</button>
     </div>
   );
 }
 
-export default App;
+
+
+function App1(){
+  const [show, setShow] = useState(false)
+  return (
+    <div style = {{padding: 20}}>
+      <button onClick = {()=>{
+        setShow(!show)
+      }}>Switch</button>
+
+      { show && <Content />}
+    </div>
+  )
+}
+
+export default App1;
