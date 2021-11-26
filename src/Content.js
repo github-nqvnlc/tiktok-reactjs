2b67b70c61b46af88f1814c10aecce54d13f7112
github-nqvnlc <locnvgcd18350@fpt.edu.vn>
2021-11-26 21:29:39 +0700

    useEffect resize

--- src/Content.js
+++ src/Content.js
@@ -1,30 +1,20 @@
 import {useEffect ,useState} from "react"
 
-
 function Content(){
+    const [width, setWidth] = useState(window.innerWidth)
 
-    const [title, setTitle] = useState('')
-    const [posts, setPosts] = useState([])
-
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(res => res.json())
-            .then(posts =>{
-                setPosts(posts)
-            })
+    useEffect(() => {
+        const handleResize = () => {
+            setWidth(window.innerWidth)
+        }
+        window.addEventListener('resize', handleResize)
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
     }, [])
     return(
         <div>
-            <input 
-                value={title}
-                onChange={e => setTitle(e.target.value)}
-            />
-            <ul>
-                {posts.map(post => (
-                    <li key={post.id}>{post.title}</li>
-
-                ))}
-            </ul>
+            <h1>{width}</h1>
         </div>
     )
 }
