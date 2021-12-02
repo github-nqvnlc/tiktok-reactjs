fcede31c4e95f64dbffea62ad96a238aec0604ea
github-nqvnlc <locnvgcd18350@fpt.edu.vn>
2021-12-02 11:39:29 +0700

    useRef()

--- src/App.js
+++ src/App.js
@@ -49,4 +49,4 @@ function App1(){
   )
 }
 
-export default App1;
+export default App;
--- src/Content.js
+++ src/Content.js
@@ -2,16 +2,28 @@ import { useEffect, useRef, useLayoutEffect, useState } from "react"
 
 function Content() {
     const [count, setCount] = useState(60)
-    let timerId = useRef()
+
+    const timerId = useRef()
+    const prevCount = useRef()
+
+    useEffect(()=>{
+        prevCount.current = count
+    },[count])
+
     const handleStart = () => {
         timerId.current = setInterval(() => {
             setCount(prevCount => prevCount - 1)
         }, 1000)
+
+        console.log('Start -> ', timerId.current);
     }
 
     const handleStop = () => {
         clearInterval(timerId.current)
+        console.log('Stop -> ', timerId.current);
     }
+
+    console.log(count, prevCount.current);
 
     return (
         <div style={{ padding: 20 }}>
