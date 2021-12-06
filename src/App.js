c5399db76722c8ef62de162788e1fa99c0782a89
github-nqvnlc <locnvgcd18350@fpt.edu.vn>
2021-12-06 10:08:12 +0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,53 +1,31 @@
-import { useState, useMemo, useRef} from "react";
+import {Routes, Route, Link} from 'react-router-dom'
+import HomePage from './Pages/Home'
+import NewsPage from './Pages/News'
+import ContactPage from './Pages/Contact'
+
 function App() {
-  const [name, setName] = useState('')
-  const [price, setPrice] = useState('')
-  const [product, setProduce] = useState([])
-
-  const nameRef = useRef()
-
-  const handleSumit = () => {
-    setProduce([...product, {
-      name,
-      price: parseInt(price)
-    }])
-    setName('')
-    setPrice('')
-    nameRef.current.focus()
-  }
-
-  const total = useMemo(()=> {
-    const result = product.reduce((result, prod) => result + prod.price, 0)
-    return result
-  },[product])
-
-  return (
-    <div style={{ padding: '10px 32px' }}>
-      <input
-        ref={nameRef}
-        value={name}
-        placeholder="Enter name"
-        onChange={e => setName(e.target.value)}
-      />
-      <br />
-      <input
-        value={price}
-        placeholder="Enter price"
-        onChange={e => setPrice(e.target.value)}
-      />
-      <br />
-      <button onClick={handleSumit}>Add</button>
-      <br />
-      Total: {total}
-      <ul>
-        {
-          product.map((product, index) => (
-            <li key={index}>{product.name} - {product.price}</li>
-          ))
-        }
-      </ul>
-    </div>
-  )
+return (
+  <div>
+    <nav>
+      <>
+        <button>
+          <Link to="/">Home</Link>
+        </button>
+        <button>
+          <Link to="/news">News</Link>
+        </button>
+        <button>
+          <Link to="/contact">Contact</Link>
+        </button>
+      </>
+    </nav>
+    <Routes>
+      <Route path="/" element={<HomePage />}/>
+      <Route path="/news" element={<NewsPage />}/>
+      <Route path="/contact" element={<ContactPage />}/>
+    </Routes>
+  </div>
+);
 }
 
 export default App;
